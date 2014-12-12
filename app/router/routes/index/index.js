64b0fdf50f8b5f4905bbbdaa5b419b9158af46aa
Luke Murphy <lukewm@riseup.net>
2014-12-11 23:28:17 -0300

    clean up index route code.
    remove logs + fix indentation

--- app/router/routes/index/index.js
+++ app/router/routes/index/index.js
@@ -2,16 +2,14 @@ var express = require('express'),
     router = express.Router(),
     Prismic = require('prismic.io').Prismic,
     prismicHelper = require('../../../assets/javascripts/prismic/prismic-helpers');
-router.get('/', prismicHelper.route(function(req, res, ctx) {
-  console.log('we are in the router get function');  
+
+router.get('/', prismicHelper.route(function(req, res, ctx){
   ctx.api.form('everything')
-         .query(Prismic.Predicates.at("document.type", "blog"))
-         .ref(ctx.ref).submit(function(err, docs) {
-    if (err) { prismicHelper.onPrismicError(err, req, res); return; }
-    res.render('index', {
-      docs: docs
+    .query(Prismic.Predicates.at("document.type", "blog"))
+    .ref(ctx.ref).submit(function(err, docs){
+      if (err){ prismicHelper.onPrismicError(err, req, res); return; }
+      res.render('index', { docs: docs });
     });
-  });
 }));
 
 module.exports = router;
